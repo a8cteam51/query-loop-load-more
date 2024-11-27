076fa479e9c1ef0d9b75f3bc6a5e93f6e3257629
Chris Bell <chrisbcodeable@gmail.com>
2024-11-27 16:44:35 +0000

    feat: add `no more posts` text functionality

--- assets/js/build/frontend.asset.php
+++ assets/js/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '4d03c2dea9d76776c78d');
+<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '7db5e5c3efc9afeb58a0');

a8ea244f9a39b21b5fe2dfc68799f63e9032bff0
Robert Mueller <robrobsn@gmail.com>
2025-03-11 16:27:57 -0400

    Made infinite scroll loader visible in the editor

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'f87891138fc05415fb3b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '2c9a39a9e88d23ce0151');

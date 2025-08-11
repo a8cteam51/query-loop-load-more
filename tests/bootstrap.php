40a5db756d886b81dd9d1db98eadbf773334d86a
James Shakespeare <j@jshakespeare.com>
2025-08-11 17:09:13 +0100

    newlines

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,26 +12,26 @@ if (!defined('WPCOMSP_QLLM_DIR_PATH')) {
 // Mock minimal WordPress HTML processor
 class WP_HTML_Tag_Processor {
     private $html;
-    
+
     public function __construct($html) {
         $this->html = $html;
     }
-    
+
     public function next_tag($args = []) {
         if (isset($args['class_name']) && $args['class_name'] === 'wp-block-query') {
             return strpos($this->html, 'wp-block-query') !== false;
         }
         return false;
     }
-    
+
     public function set_attribute($name, $value) {
         $this->html = str_replace(
-            '<div class="wp-block-query"', 
-            "<div class=\"wp-block-query\" {$name}=\"{$value}\"", 
+            '<div class="wp-block-query"',
+            "<div class=\"wp-block-query\" {$name}=\"{$value}\"",
             $this->html
         );
     }
-    
+
     public function get_updated_html() {
         return $this->html;
     }
